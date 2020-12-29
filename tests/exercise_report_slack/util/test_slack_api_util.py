940aaf12c3c9fa70c13cd2a5123b75e0ddb6402d
yamap55 <tetuya120@hotmail.com>
2020-12-29 11:49:11 +0900

    [refactor] test case

--- tests/exercise_report_slack/util/test_slack_api_util.py
+++ tests/exercise_report_slack/util/test_slack_api_util.py
@@ -6,45 +6,56 @@ from slack_sdk.errors import SlackApiError
 
 
 class TestGetUserName:
-    @mock.patch("exercise_report_slack.util.slack_api_util.client.users_info")
-    def test_nomal_case(self, mock_client):
-        mock_client.return_value = {"user": {"real_name": "REAL_NAME"}}
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        with mock.patch(
+            "exercise_report_slack.util.slack_api_util.client.users_info",
+        ) as mock_method:
+            self.mock_method = mock_method
+            yield
+
+    def test_nomal_case(self):
+        self.mock_method.return_value = {"user": {"real_name": "REAL_NAME"}}
 
         actual = get_user_name("USER_ID")
         expected = "REAL_NAME"
 
         assert actual == expected
-        mock_client.assert_called_once_with(user="USER_ID")
+        self.mock_method.assert_called_once_with(user="USER_ID")
 
-    @mock.patch("exercise_report_slack.util.slack_api_util.client.users_info")
-    def test_not_exists_user_id(self, mock_client):
+    def test_not_exists_user_id(self):
         # APIにアクセスしたくないため、モックで例外を投げている
         # ユニットテストとしては意味がないが、仕様記載の意味で記載しておく
-        mock_client.side_effect = SlackApiError("a", "b")
+        self.mock_method.side_effect = SlackApiError("a", "b")
 
         with pytest.raises(SlackApiError):
             get_user_name("NOT_EXISTS_USER_ID")
 
 
 class TestGetChannelId:
-    @mock.patch("exercise_report_slack.util.slack_api_util.client.conversations_list")
-    def test_nomal_case(self, mock_client):
-        mock_client.return_value = {
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        with mock.patch(
+            "exercise_report_slack.util.slack_api_util.client.conversations_list",
+        ) as mock_method:
+            self.mock_method = mock_method
+            yield
+
+    def test_nomal_case(self):
+        self.mock_method.return_value = {
             "channels": [
                 {"name": "CHANNEL_NAME1", "id": "CHANNEL_ID1"},
                 {"name": "CHANNEL_NAME2", "id": "CHANNEL_ID2"},
             ]
         }
-
         actual = get_channel_id("CHANNEL_NAME1")
         expected = "CHANNEL_ID1"
 
         assert actual == expected
-        mock_client.assert_called_once_with()
+        self.mock_method.assert_called_once_with()
 
-    @mock.patch("exercise_report_slack.util.slack_api_util.client.conversations_list")
-    def test_not_exists_channel_name(self, mock_client):
-        mock_client.return_value = {"channels": []}
+    def test_not_exists_channel_name(self):
+        self.mock_method.return_value = {"channels": []}
 
         with pytest.raises(ValueError):
             get_channel_id("CHANNEL_NAME")
